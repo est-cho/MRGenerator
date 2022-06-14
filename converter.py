24e31806f1c73e33fa59e0a85733fe2a411cd0f1
Esther.Cho <esther.mk.cho@gmail.com>
2022-06-14 17:33:21 +0900

    Added representation of MRs. Added extraction of initial MRs from xml file.

--- converter.py
+++ converter.py
@@ -1,9 +1,60 @@
+import xml.etree.ElementTree as ET
+import representation as MR
+
+TAG_BODY = 'body'
+TAG_INPUT = 'input'
+TAG_STATEMENT = 'statement'
+TAG_PROP = 'prop'
+TAG_VALUE = 'value'
+TAG_TYPE = 'type'
+TAG_INDEX = 'index'
+TAG_TIME = 'time'
+TAG_OP = 'op'
+
+ATTRIB_NAME = 'name'
+ATTRIB_LEFT = 'left'
+ATTRIB_RIGHT = 'right'
+
+VAL_TYPE_VAR = 'v'
+VAL_TYPE_CONS = 'c'
 
 
 def parse_mr_xml(file_name):
+    root = ET.parse(file_name).getroot()
+
+    s_list = []
+    for s in root.findall('statement'):  # Find all statements
+        index = int(s.attrib[ATTRIB_NAME].split('_')[1])
+        lp = MR.Prop()
+        rp = MR.Prop()
+        for p in s.findall(TAG_PROP):  # Find all propositions
+            lv = MR.Value()
+            rv = MR.Value()
+            for pf in p.findall(TAG_VALUE):  # Find left and right values in the proposition
+                if pf.attrib[ATTRIB_NAME] == ATTRIB_LEFT:
+                    if pf.find(TAG_TIME) is None:
+                        lv = MR.Value(pf.find(TAG_TYPE).text, int(pf.find(TAG_INDEX).text))
+                    else:
+                        lv = MR.Value(pf.find(TAG_TYPE).text, int(pf.find(TAG_INDEX).text),
+                                      int(pf.find(TAG_TIME).text))
+                else:
+                    if pf.find(TAG_TIME) is None:
+                        rv = MR.Value(pf.find(TAG_TYPE).text, int(pf.find(TAG_INDEX).text))
+                    else:
+                        rv = MR.Value(pf.find(TAG_TYPE).text, int(pf.find(TAG_INDEX).text),
+                                      int(pf.find(TAG_TIME).text))
+            op = p.find(TAG_OP).text
+
+            if p.attrib[ATTRIB_NAME] == ATTRIB_LEFT:
+                lp = MR.Prop(lv, op, rv)
+            else:
+                rp = MR.Prop(lv, op, rv)
+
+        statement = MR.Statement(index, lp, rp)
+        s_list.append(statement)
+
+    return s_list
+
+
+def write_mr_to_csv(generated_mr, output_file_name=""):
     return True
-
-
-def write_mr_to_csv(generated_mr):
-    return True
-
---
+++ representation.py
@@ -0,0 +1,53 @@
+from dataclasses import dataclass
+import operator
+
+
+
+OPERATORS = ["==", "<=", ">=", "<", ">", "!="]
+OPERATOR_DICT = {"==": operator.eq, "<=": operator.le, ">=": operator.ge,
+                 "<": operator.lt, ">": operator.gt, "!=": operator.ne}
+
+
+@dataclass
+class Value:
+    type: str = ''
+    index: int = 0
+    time: int = None
+
+    def copy(self):
+        copy = Value()
+        copy.type = self.type
+        copy.index = self.index
+        copy.time = self.time
+        return copy
+
+
+@dataclass
+class Prop:
+    v_left: Value = Value()
+    op: str = ''
+    v_right: Value = Value()
+
+    def copy(self):
+        copy = Prop()
+        copy.v_left = self.v_left.copy()
+        copy.op = self.op
+        copy.v_right = self.v_right.copy()
+        return copy
+
+
+@dataclass
+class Statement:
+    index: int = 0
+    p_left: Prop = Prop()
+    p_right: Prop = Prop()
+
+    def copy(self):
+        copy = Statement()
+        copy.index = self.index
+        copy.p_left = self.p_left.copy()
+        copy.p_right = self.p_right.copy()
+        return copy
+
+
+
